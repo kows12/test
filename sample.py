777a2a1920da2e73aadd9efef0901664fecd7078
kows <kowsiec1827@gmail.com>
2023-05-12 15:26:52 +0530

    calc

---
+++ sample.py
@@ -0,0 +1,28 @@
+# Define the function for the calculator
+def calculator():
+    # Get user input for the first number
+    num1 = float(input("Enter the first number: "))
+
+    # Get user input for the second number
+    num2 = float(input("Enter the second number: "))
+
+    # Get user input for the operation
+    operation = input("Enter the operation (+, -, *, /): ")
+
+    # Perform the operation based on user input
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        result = num1 / num2
+    else:
+        print("Invalid operation. Please enter a valid operation.")
+
+    # Print the result
+    print("The result is: ", result)
+
+# Call the calculator function
+calculator()
